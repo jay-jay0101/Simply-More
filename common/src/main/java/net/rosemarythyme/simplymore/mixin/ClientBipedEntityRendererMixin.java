4f500995e59f3fac81916e8c8a87cdbd0cb99db9
RosemaryThyme <138990366+jay-jay0101@users.noreply.github.com>
2026-08-18 19:55:25 +0100

    Removed armor from statues

---
+++ common/src/main/java/net/rosemarythyme/simplymore/mixin/ClientBipedEntityRendererMixin.java
@@ -0,0 +1,23 @@
+package net.rosemarythyme.simplymore.mixin;
+
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.feature.ArmorFeatureRenderer;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.rosemarythyme.simplymore.client.render.entity.StatueRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ArmorFeatureRenderer.class)
+public abstract class ClientBipedEntityRendererMixin<T extends LivingEntity, M extends BipedEntityModel<T>, A extends BipedEntityModel<T>> {
+    @Inject(method = "renderArmor", at = @At("HEAD"), cancellable = true)
+    private void simplymore$statue(MatrixStack matrices, VertexConsumerProvider vertexConsumers, T entity, EquipmentSlot armorSlot, int light, A model, CallbackInfo ci) {
+        if (StatueRenderer.shouldRenderAsStatue()) {
+            ci.cancel();
+        }
+    }
+}
