66526b26ac94e16260f6f22b99a0655deeeb172d
RosemaryThyme <138990366+jay-jay0101@users.noreply.github.com>
2026-09-23 09:00:28 +0100

    Fixed Statue not rendering

--- common/src/main/java/net/rosemarythyme/simplymore/mixin/ClientLivingEntityRendererMixin.java
+++ common/src/main/java/net/rosemarythyme/simplymore/mixin/ClientLivingEntityRendererMixin.java
@@ -13,16 +13,12 @@ import net.rosemarythyme.simplymore.util.MathUtils;
 import net.rosemarythyme.simplymore.world.ActiveAbilityManager;
 import net.rosemarythyme.simplymore.world.ClientActiveAbilityManager;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntityRenderer.class)
 public abstract class ClientLivingEntityRendererMixin<T extends LivingEntity> {
-    @Shadow
-    public abstract void render(T arg, float g, float h, MatrixStack arg2, VertexConsumerProvider arg3, int j);
-
     @Inject(method = "setupTransforms", at = @At("TAIL"))
     private void simplymore$transform(T entity, MatrixStack matrices, float animationProgress, float bodyYaw, float tickDelta, float scale, CallbackInfo ci) {
         double currentDuration = ClientActiveAbilityManager.CLIENT.getCurrentDuration(entity, ActiveAbilityManager.Type.DRILL);
@@ -46,7 +42,7 @@ public abstract class ClientLivingEntityRendererMixin<T extends LivingEntity> {
 
     @Inject(method = "render(Lnet/minecraft/entity/LivingEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At("HEAD"), cancellable = true)
     private void simplymore$stopRender(T livingEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
-        if (ClientActiveAbilityManager.CLIENT.isStatue(livingEntity)) {
+        if (!StatueRenderer.shouldRenderAsStatue() && ClientActiveAbilityManager.CLIENT.isStatue(livingEntity)) {
             ci.cancel();
             return;
         }
@@ -59,6 +55,14 @@ public abstract class ClientLivingEntityRendererMixin<T extends LivingEntity> {
         double ticksUsed = duration - drillDuration;
 
         if (drillDuration > 0 && ticksUsed > 10) ci.cancel();
+    }
+
+    @Inject(method = "render(Lnet/minecraft/entity/LivingEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/feature/FeatureRenderer;render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/entity/Entity;FFFFFF)V"), cancellable = true)
+    private void stopFeatureRendering(T livingEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
+        if(StatueRenderer.shouldRenderAsStatue()) {
+            matrixStack.pop();
+            ci.cancel();
+        }
     }
 
     @ModifyReturnValue(method = "getShadowRadius(Lnet/minecraft/entity/LivingEntity;)F", at = @At("TAIL"))
